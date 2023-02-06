84a3162da99b371cc30128c20a19c5568c51f84b
MarkJamesHoward <markjameshoward@hotmail.co.uk>
2023-02-06 16:56:47 +1300

    change name to app

--- bin/index.js
+++ bin/index.js
@@ -1,16 +1,14 @@
 #!/usr/bin/env node
 const plugin = "@markjameshoward/say-hi-plugin";
 function SayHiStandard() {
-    return "Standard Say hi";
+  return "Standard Say hi";
 }
-let SayHiPlugin = {
-    GetHi: () => SayHiStandard(),
+let app = {
+  GetHi: () => SayHiStandard(),
 };
 try {
-    SayHiPlugin = await import(plugin);
+  app = await import(plugin);
+} catch (e) {
+  console.log("no plugins found - using builtin");
 }
-catch (e) {
-    console.log("no plugins found - using builtin");
-}
-console.log(SayHiPlugin.GetHi());
-export {};
+console.log(app.GetHi());
